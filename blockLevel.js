c0ca6e0a6c6ae7c71691ad464e209f052cafa54c
Ryanmkbj <Ryanburkejimenez@gmail.com>
2023-09-24 20:48:49 -0700

    added Scope exercises

---
+++ blockLevel.js
@@ -0,0 +1,8 @@
+{
+    let blockLevelVariable = "Block Level";
+    // This is a block level variable 
+    console.log(blockLevelVariable);
+    //Prints "Block Level" to console
+}
+
+//console.log(blockLevelVariable);
---
+++ localLevel.js
@@ -0,0 +1,8 @@
+function displayVar(){
+    let localLevelVariable = "Local Level";
+    //This is a local level variable; only accessed within the function
+    console.log(localLevelVariable);
+}
+
+displayVar();
+//console.log(localLevelVariable);
